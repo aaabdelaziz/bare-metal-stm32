07a81ccdca899fc4350462c86baa0b7dd1d87911
AAbdelaziz <eng.aaaziz@gmail.com>
2025-03-20 18:19:39 +0100

    Create first AHB1/AHB2, GPIOA address mapping.

---
+++ 0-GPIO/Src/main.c
@@ -0,0 +1,18 @@
+
+// For Buses Peripheral Addresses
+#define PERIPH_BASE           (0x40000000UL)
+#define AHB2_PERIPH_OFFSET    (0x0800 0000UL)
+#define AHB2_PERIPH_BASE      (PERIPH_BASE + AHB2_PERIPH_OFFSET)
+
+#define AHB1_PERIPH_OFFSET    (0x20000UL)
+#define AHB1_PERIPH_BASE      (PERIPH_BASE + AHB1_PERIPH_OFFSET)
+
+
+// For GPIO
+#define GPIOA_OFFSET          (0x0000U)    // 0x0000 0000
+#define GPIO_BASE 			  (AHB2_PERIPH_BASE + GPIOA_OFFSET)
+
+
+// For Clock
+#define RCC_OFFSET    		  (0x21000UL)
+#define RCC_BASE 			  (PERIPH_BASE + RCC_OFFSET)
