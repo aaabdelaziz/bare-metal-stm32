53d0e1e44ac25017eba7b5ce8ca46f64203a2ffc
aaabdelaziz <aiaaziz.sw@gmail.com>
2025-04-28 01:06:24 +0200

    Update main file to set GPIOA, RCC registers using pointers.

--- 0-LedToggling/Src/main.c
+++ 0-LedToggling/Src/main.c
@@ -7,16 +7,16 @@ int main ()
 
 
 	/* (1) Enable the peripheral clock of GPIOA */
-	RCC_AHB1EN_R |= GPIOAEN;
+	RCC->AHBENR |= GPIOAEN;
 
 	/* (2) Set PA5 as Output PIN */
-	GPIOA_MODE_R |= (1U << 10);    // Set bit 10 to 1
-	GPIOA_MODE_R &= ~(1U << 11);    // Set bit 11 to 0
+	GPIOA->MODER  |= (1U << 10);     // Set bit 10 to 1
+	GPIOA->MODER  &= ~(1U << 11);    // Set bit 11 to 0
 
 	while(1)
 	{
 		/* (3) Toggle PA5 */
-		GPIOA_ODR_R  ^= LED_PIN_NUM;
+		GPIOA->ODR  ^= LED_PIN_NUM;
     	for(int i=0; i<100000;i++);
 	}
 
