82dc383e73a7c1aaf1773d49865862f26140a629
aaabdelaziz <aiaaziz.sw@gmail.com>
2025-05-05 14:50:25 +0200

    Configured GPIOC/Pin13  for input button, and the GPIOA/Pin5 as Led output.

--- 1-GPIO_output/Src/main.c
+++ 1-GPIO_output/Src/main.c
@@ -4,28 +4,48 @@
 
 // Enable GPIOA clock
 #define GPIOAEN      	 (1U<<17)
+// Enable GPIOC clock
+#define GPIOCEN      	 (1U<<21)
 
-//PIN5 SET/REST
+//Output: PIN5 SET/REST
 #define LED_PIN_NUM_SET      (1U<<5)
 #define LED_PIN_NUM_REST   	 (1U<<21)
+//Input Button: PIN13 
+#define BTN_PIN_NUM          (1U<<13)
 
 
 int main(void)
 {
 
+	//Enable GPIOA, GPIOC Clock
 	RCC->AHBENR |= GPIOAEN;
+	RCC->AHBENR |= GPIOCEN;
 
+	// Config GPIOA-PIN5 as Output
 	GPIOA->MODER |= (1U<<10);
 	GPIOA->MODER &=~ (1U<<11);
 
+	//  Config GPIOC-PIN13 as Input
+	GPIOC->MODER &=~ (1U<<26);
+	GPIOC->MODER &=~ (1U<<27);
+
+
 	while(1)
 	{
-		// Enable the register
-		GPIOA->BSRR = LED_PIN_NUM_SET;
-		for(int i=0; i<10000;i++){}
+		// Checking if Button is pressed, turn on LED, otherwisse turn it off
+		if(GPIOC->IDR & BTN_PIN_NUM)
+		{
 
-		GPIOA->BSRR = LED_PIN_NUM_REST;
-		for(int i=0; i<10000;i++){}
+			// Turn Led ON
+			GPIOA->BSRR = LED_PIN_NUM_SET;
+			for(int i=0; i<10000;i++){}
+		}
+		else
+		{
+			// Turn Led Off
+			GPIOA->BSRR = LED_PIN_NUM_REST;
+			for(int i=0; i<10000;i++){}
+		}
 	}
 
 	return 0;
