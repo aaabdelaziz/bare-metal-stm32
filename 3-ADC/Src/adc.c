1523b0b5efa0bc2274157c63376ff07b65b8bcdb
aaabdelaziz <aiaaziz.sw@gmail.com>
2025-07-08 11:51:38 +0200

    Remove incorrect configurations; seems doesn't work, and restructure the ADC Channel configuration step by step.

--- 3-ADC/Src/adc.c
+++ 3-ADC/Src/adc.c
@@ -7,21 +7,16 @@
 
 #include "adc.h"
 
-
 /**
  * @brief Initialize GPIO for ADC channel (e.g., PA1)
  */
 void pa1_adc_init(void)
 {
     /* Step 1: Enable clock for GPIOA */
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+
 
     /* Step 2: Configure PA1 as analog input (ADC input) */
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_1;                // PA1
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;         // set to analog mode
-    GPIO_InitStruct.Pull = GPIO_NOPULL;              // no pull-up or pull-down
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 
@@ -31,29 +26,10 @@ void pa1_adc_init(void)
 void adc1_init(void)
 {
     /* Step 1: Enable clock for ADC1 */
-    __HAL_RCC_ADC1_CLK_ENABLE();
 
     /* Step 2: Configure the ADC peripheral */
-    hadc1.Instance = ADC1;
 
-    hadc1.Init.OversamplingMode = DISABLE;                                         // oversampling disabled
-    hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;                          // set clock divider
-    hadc1.Init.Resolution = ADC_RESOLUTION_12B;                                    // 12-bit resolution
-    hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;                                    // single channel
-    hadc1.Init.ContinuousConvMode = DISABLE;                                       // one shot mode
-    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;               // software trigger
-    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;                                    // right aligned data
-    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;                                 // EOC after each conversion
-    hadc1.Init.DMAContinuousRequests = DISABLE;                                    // no DMA
-    hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;                                   // preserve data if overrun occurs
-
-    if (HAL_ADC_Init(&hadc1) != HAL_OK)
-    {
-        /* Initialization Error */
-        Error_Handler();
-    }
 }
-
 
 
 /**
@@ -61,16 +37,6 @@ void adc1_init(void)
  */
 void adc1_channel_config(void)
 {
-    ADC_ChannelConfTypeDef sConfig = {0};
-
     /* Configure the selected ADC channel */
-    sConfig.Channel = ADC_CHANNEL_1;                      // Channel 1 (PA1)
-    sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;               // Rank in the sequence
-    sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;     // Sampling time
 
-    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-    {
-        /* Configuration Error */
-        Error_Handler();
-    }
 }
--- 3-ADC/Src/main.c
+++ 3-ADC/Src/main.c
@@ -3,18 +3,15 @@
 
 // Define GPIOA clock enable macro
 #define GPIOAEN    (1U<<17)   // Bit 17: I/O port A clock enable
-#define UART2EN    (1U<<17)   // Bit 17: USART2 clock enable
 
-// Baudrate calculation (for 9600 baud with 8 MHz PCLK)
-#define SYS_FREQ     8000000
-#define APB1_CLK     SYS_FREQ
-#define UART_BAUDRATE 9600
 
 int main(void)
 {
 
     while (1)
     {
+
+
 
     }
 
